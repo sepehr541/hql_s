415c6246526edcdef650be5f0aa00360269d4363
sepehr541 <sepehr541@gmail.com>
2020-03-04 15:10:29 -0800

    skeleton for postgreSQL handling

--- app.js
+++ app.js
@@ -7,7 +7,61 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const { Pool } = require('pg');
+
 var app = express();
+
+// Connection to PostgreSQL on Docker container
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'docker',
+  port: 5432,
+})
+
+/**
+ * TESTING CONNECTION
+ */
+
+// pool.query('SELECT $1::text as name', ['brianc'], (err, result) => {
+//   if (err) {
+//     return console.error('Error executing query', err.stack)
+//   }
+//   console.log(result.rows[0].name) // brianc
+// })
+
+// pool.query(`CREATE TABLE test (
+//             id INTEGER, name CHAR(20))`,
+//           (err, result) => {
+//             if (err) {
+//               return console.error('Error executing query', err.stack)
+//             }
+//             else {
+//               console.log("Table should be made");
+//             }
+// });
+
+
+// pool.query(`INSERT INTO test(id, name) VALUES (20, 'Tom')`, (err, result) => {
+//   if (err) {
+//     return console.error('Error executing query', err.stack)
+//   }
+//   else {
+//     console.log("Tom should now be in the table");
+//   }
+// })
+
+pool.query(`SELECT * FROM pg_stat_activity`, (err, result) => {
+  if (err) {
+    return console.log(err.stack);
+  } 
+  else {
+    console.log(result.rows);
+  }
+})
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
