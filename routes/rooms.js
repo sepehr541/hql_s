74ae21e6cab67e4703f5ab6d946ad1041d9032bb
sepehr541 <sepehr541@gmail.com>
2020-03-07 00:01:27 -0800

    body-parser setup

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,2 +1,9 @@
 /* GET rooms */
 const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res, next)=> {
+  const data=req.body
+});
+
+module.exports = router;
