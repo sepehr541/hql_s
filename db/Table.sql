315839ce4d8e19c94e72abbfafd0f46aab8eb06c
amirhoseyn221177 <amirhoseyn221177>
2020-03-06 21:12:44 -0800

    Table creation queries moved to Table.sql

---
+++ db/Table.sql
@@ -0,0 +1,53 @@
+CREATE TABLE Parking (
+    Pnum INTEGER PRIMARY KEY
+);
+
+Create TABLE Room (
+        ID INTEGER PRIMARY KEY,
+        Capacity INTEGER,
+        BedType CHAR VARYING (20),
+);
+
+Create TABLE Visitor (
+        Visitor_ID INTEGER PRIMARY KEY,
+        CardNumber INTEGER,
+        Name VARCHAR(20)
+ );
+
+ Create TABLE Employee (
+            ID INTEGER PRIMARY KEY,
+            Name CHAR(20),
+            Salary INTEGER,
+            Position CHAR(20)
+);
+
+Create TABLE Recreational (
+            ID INTEGER PRIMARY KEY,
+            Type CHAR(20)
+            
+);
+
+Create TABLE Restaurant (
+            Name CHAR(20) PRIMARY KEY,
+            CuisineStyle CHAR(20) FOREIGN KEY REFERENCES Food 
+            ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+ );
+
+ Create TABLE Food (
+            CuisineStyle CHAR(20) PRIMARY KEY,
+            Category CHAR(20),
+ );
+
+ Create TABLE Customer_Service (
+            ID INTEGER,
+            Type CHAR(20),
+            Hours INTEGER,
+);
+
+Create TABLE RoomEssentials (
+            itemID INTEGER,
+            Name CHAR(20),
+            Price INTEGER,
+);
+
+
