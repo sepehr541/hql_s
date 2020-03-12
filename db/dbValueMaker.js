62951b1f9f50f7b8f3b97b458a0e92e59801ab42
sepehr541 <sepehr541@gmail.com>
2020-03-12 00:51:59 -0700

    fixed bedtype query error

--- db/dbValueMaker.js
+++ db/dbValueMaker.js
@@ -1,3 +1,7 @@
 const makeRoomValues = (id, cap, bedtype) => {
     return `(${id}, ${cap}, ${bedtype})`
 }
+
+const makeResvValues = (id, startDate, endDate, price, pCount) => {
+    return `(${id}, ${startDate.format('YYYY-MM-DD')} , ${endDate.formatformat('YYYY-MM-DD')}, ${price}, ${pCount})`;
+}
