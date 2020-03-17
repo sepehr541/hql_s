71deacb02674ddd441efe4d983c424e8b97ff04a
sepehr541 <sepehr541@gmail.com>
2020-03-16 19:03:35 -0700

    pricing added to roomfinder query

--- db/selectQueries.js
+++ db/selectQueries.js
@@ -11,9 +11,11 @@ const FindingAvailableRooms = async (start, end, people) => {
     let available_rooms = []
     try {
         const resp = await pool.query(
-           `select id roomnumber, bedtype
-            from room 
-            where room.capacity = ${people}  AND not exists (
+           `select id roomnumber, bedtype, pr.price
+            from room , roomprice pr
+            where room.capacity = ${people} 
+            AND pr.roomtype = room.bedtype
+            AND not exists (
                 select * from reservationroom 
                 where room.id = reservationroom.roomnumber)
             UNION
@@ -23,7 +25,7 @@ const FindingAvailableRooms = async (start, end, people) => {
                 AND (('${start}' :: date  >= res.enddate) OR (res.startdate >= '${end}':: date)) 
                 AND room.id = rr.RoomNumber 
                 AND (res.pcount = ${people})
-                AND pr.roomtype=room.bedtype`);
+                AND pr.roomtype = room.bedtype`);
         available_rooms = resp.rows;
     } catch (e) {
         console.log(e);
