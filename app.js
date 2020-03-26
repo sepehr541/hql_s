7f5c4c60747be10f8a30f1dd0f5ae7336251b656
amirhoseyn221177 <amirhoseyn221177>
2020-03-26 04:20:45 -0700

    added

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ var reservation = require("./routes/resv")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var roomsRouter = require('./routes/rooms');
+var confirmedResv=require('./routes/confirmedresv')
 var cors=require("cors")
 
 var app = express();
 
-app.use(logger('dev'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');app.use(logger('dev'));
 app.use(express.json());
 app.use(cors())
 app.use(body_parser.json())
@@ -23,7 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/rooms', roomsRouter);
-app.use("/reservation", reservation)
+app.use('/reservation', reservation);
+// app.use('/confirmed',confirmedResv)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
