98c80adc7b05e9d358e38a5150165d84bed75b9e
amirhoseyn221177 <amirsayuar221177@gmail.com>
2020-03-26 23:06:18 -0700

    final resereve added

--- routes/finalReservation.js
+++ routes/finalReservation.js
@@ -12,7 +12,7 @@ route.post("/", async(req,res)=>{
     let  people = await data.people
     const roomid= await data.roomid
     const phone = await data.phone
-    const email= data.email
+    const email= await data.email
     people= parseInt(people)
     console.log(name)
     console.log(start)
