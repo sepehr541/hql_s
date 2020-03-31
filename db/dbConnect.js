1ac160382d8e6eba6b2cc58e8c4c2af0374057e2
sepehr541 <sepehr541@gmail.com>
2020-03-30 21:33:49 -0700

    dfghj

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const auth = require('../public/auth/auth')
+const auth = require('../auth/auth')
 // Connection to PostgreSQL on Docker container
 // TODO
 const pool = new Pool({...auth});
