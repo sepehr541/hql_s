9477c7cdedd8444ac1c4a73074111b906080360e
sepehr541 <sepehr541@gmail.com>
2020-03-31 21:12:38 -0700

    finish

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/rooms', roomsRouter);
